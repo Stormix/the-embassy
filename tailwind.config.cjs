424a204f48680423cf6c7f8956e7de22631b9f11
Anas Mazouni <madadj4@gmail.com>
2023-09-03 17:55:40 +0200

    feat(hero): implemented hero section

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,23 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    colors: {
+      background: 'rgb(var(--background) / <alpha-value>)',
+      primary: 'rgb(var(--primary) / <alpha-value>)',
+      secondary: 'rgb(var(--secondary) / <alpha-value>)'
+    },
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+      backgroundImage: {
+        'code': "url('/images/code.gif')",
+        'design': "url('/images/design.gif')",
+      }
+    }
+  },
+  plugins: []
+};
